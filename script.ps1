7bf9b965fc1add5d4778ebd0a55550aacf33ad90
wenchiehlee <wjlee@csie.nctu.edu.tw>
2024-08-02 15:29:18 +0800

    try to fix privalige problem..

--- script.ps1
+++ script.ps1
@@ -1,6 +1,10 @@
 
-$env:COMPUTERNAME
-$env:USERNAME
-$env:USERDNSDOMAIN
+Write-Output $env:COMPUTERNAME     -ForegroundColor Magenta
+Write-Output $env:USERNAME         -ForegroundColor Magenta
+Write-Output $env:USERDNSDOMAIN    -ForegroundColor Magenta
 
+whoami
+
+sc.exe queryex type= service state= all | find /i "actions.runner"
+# sc config "actions.runner.wenchiehlee-Win11Deploy.TAICLTB37TMOCQ7" obj= "NT AUTHORITY\SYSTEM" type= own
 choco upgrade all -y
