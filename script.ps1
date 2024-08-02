3e5d80945c94606fd663f9e1d1c7bdad6f085700
wenchiehlee <wjlee@csie.nctu.edu.tw>
2024-08-02 16:06:11 +0800

    fix sc parameter issue

--- script.ps1
+++ script.ps1
@@ -1,10 +1,10 @@
 
-Write-Output $env:COMPUTERNAME     -ForegroundColor Magenta
-Write-Output $env:USERNAME         -ForegroundColor Magenta
-Write-Output $env:USERDNSDOMAIN    -ForegroundColor Magenta
+Write-host $env:COMPUTERNAME     -ForegroundColor Magenta
+Write-host $env:USERNAME         -ForegroundColor Magenta
+Write-host $env:USERDNSDOMAIN    -ForegroundColor Magenta
 
-whoami
+whoami     | Write-host          -ForegroundColor Magenta
 
-sc.exe queryex type= service state= all | find /i "actions.runner"
+sc.exe queryex type= service state= all | Select-String -Pattern "actions.runner" -CaseSensitive -SimpleMatch
 # sc config "actions.runner.wenchiehlee-Win11Deploy.TAICLTB37TMOCQ7" obj= "NT AUTHORITY\SYSTEM" type= own
 choco upgrade all -y
